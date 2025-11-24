10f53bdcb8e18a1146cb3589533700f3bb112de7
morihirok <morihirok@st.inc>
2025-11-24 23:39:27 +0900

    うーん

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,4 +1,6 @@
 class Dashboard::OrdersController < ApplicationController
+  before_action :set_items_and_users, only: [ :new, :create ]
+
   def index
     @orders = Order.includes(:item, :reservation, :user).order(created_at: :desc)
   end
@@ -18,52 +20,36 @@ class Dashboard::OrdersController < ApplicationController
   def create
     reservation = Reservation.find(order_params[:reservation_id])
     item = Item.find(order_params[:item_id])
-
-    if item.stock < 1
-      @items = Item.all.order(:name)
-      @users = User.all.order(:name)
-      @order = Order.new(
-        reservation: reservation,
-        email: reservation.email,
-        name: reservation.name,
-        user: Current.user
-      )
-
-      flash.now.alert = "在庫が不足しています"
-      return render :new
-    end
-
     user = User.find(order_params[:user_id])
 
-    order = reservation.build_order(
+    @order = reservation.build_order(
       email: reservation.email,
       name: item.name,
       user: user,
       item: item,
     )
 
-    begin
-      response = PaymentApiClient.execute(token: order_params[:token], amount: order.item.price)
-    rescue
-      @items = Item.all.order(:name)
-      @users = User.all.order(:name)
-      flash.now.alert = "決済に失敗しました。"
+    if item.stock < 1
+      flash.now.alert = "在庫が不足しています"
       return render :new
     end
 
-    order.build_payment(payment_id: response[:payment_id], amount: response[:amount])
-
-    order.save!
-    reservation.update!(status: :completed)
-
-    item.update!(stock: item.stock - 1)
-
-    redirect_to dashboard_orders_path, notice: "注文が作成されました"
+    if @order.execute_payment_and_update_reservation_and_item(token: order_params[:token])
+      redirect_to dashboard_orders_path, notice: "注文が作成されました"
+    else
+      flash.now.alert = "決済に失敗しました。"
+      render :new
+    end
   end
 
   private
 
   def order_params
     params.require(:order).permit(:reservation_id, :item_id, :user_id, :token)
+  end
+
+  def set_items_and_users
+    @items = Item.all.order(:name)
+    @users = User.all.order(:name)
   end
 end
