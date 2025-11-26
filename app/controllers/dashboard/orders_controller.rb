49c143cbaaaca46581b2201a2a34963231583d0b
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-11-26 15:51:16 +0900

    Create payment and update item stock and reservation status
    happy path only

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -26,13 +26,37 @@ class Dashboard::OrdersController < ApplicationController
       item: item,
     )
     if order.save
+      begin
+        payment_api_client = PaymentApiClient.execute(
+          token: params[:order][:token],
+          amount: item.price
+        )
+        payment = Payment.new(
+          amount: payment_api_client[:amount],
+          payment_id: payment_api_client[:payment_id],
+          order: order,
+        )
+      rescue Timeout::Error
+        flash.alert = order.errors.full_messages.join(", ")
+        redirect_to new_dashboard_order_path(reservation_id: reservation.id)
+        return
+      end
+
+      if payment.save
+        reservation.update!(status: "completed")
+        item.decrement!(:stock)
+        redirect_to dashboard_orders_path, notice: "注文が作成されました"
+        return
+      else
+        # TODO
+      end
       redirect_to dashboard_orders_path, notice: "注文が作成されました"
     else
       @order = order
       @items = Item.all.order(:name)
       @users = User.all.order(:name)
-      flash.now.alert = order.errors.full_messages.join(", ")
-      render :new
+      flash.alert = order.errors.full_messages.join(", ")
+      redirect_to new_dashboard_order_path(reservation_id: reservation.id)
     end
   end
 end
